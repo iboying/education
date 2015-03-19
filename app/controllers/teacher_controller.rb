53b8c320c197ebc7ac170d34e71e0b9a80062c60
OnlyCZY <853852998@qq.com>
2015-03-19 21:47:48 +0800

    add teacher_test and teacher_result page

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -3,5 +3,14 @@ class TeacherController < ApplicationController
   end
 
   def result
+  	  @static_item = {
+  		"1" => "正确答案",
+  		"2" => "正确率",
+  		"3" => "作答人数",
+  		"4" => "选A人数",
+  		"5" => "选B人数",
+  		"6" => "选C人数",
+  		"7" => "选D人数"
+  	}
   end
 end
